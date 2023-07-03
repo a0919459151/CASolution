66f54c58bfec77922f11e50ad86a6b1ab08f547a
joe.chiu <a0919459151@gmail.com>
2023-07-03 16:02:04 +0800

    install mapster

--- CASolution.Api/Controllers/WeatherForecastController.cs
+++ CASolution.Api/Controllers/WeatherForecastController.cs
@@ -1,5 +1,6 @@
 using CASolution.Api.Contracts;
 using CASolution.Application.services.WeatherForecast;
+using Mapster;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CASolution.Api.Controllers;
@@ -18,20 +19,8 @@ public class WeatherForecastController : ApiController
     public async Task<IActionResult> GetWeatherForecast()
     {
         var results = await _weatherForecastService.GetWeatherForecast();
-
         return results.Match(
-            results => Ok(MapToResponse(results)),
+            results => Ok(results.Adapt<IEnumerable<GetWeatherForecastResponse>>()),
             errors => Problem(errors));
-    }
-
-    private static IEnumerable<GetWeatherForecastResponse> MapToResponse(IEnumerable<WeatherForecastResult> results)
-    {
-        return results.Select(x => new GetWeatherForecastResponse
-        {
-            Date = x.Date,
-            TemperatureC = x.TemperatureC,
-            TemperatureF = x.TemperatureF,
-            Summary = x.Summary
-        });
     }
 }
